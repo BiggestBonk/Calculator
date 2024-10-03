3868693494c93ad9164106c76621f194d428ee28
biggestbonk <harry.walsh505@gmail.com>
2024-10-03 15:18:12 +1300

    add calculator index.html add calculator main.js add calculator styles.css add calculator functions (placeholder) add html calculator table and cells add placeholder display

---
+++ main.js
@@ -0,0 +1,23 @@
+let num1
+let num2
+let operator
+
+function addition(num1, num2) {
+  return num1 + num2
+}
+
+function subtraction(num1, num2) {
+  return num1 - num2
+}
+
+function multiplication(num1, num2) {
+  return num1 * num2
+}
+
+function division(num1, num2) {
+  return num1 / num2
+}
+
+function operate(num1, operator, num2) {
+  multiplication(num1, num2)
+}
